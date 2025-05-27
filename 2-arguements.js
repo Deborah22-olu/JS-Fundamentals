dd4a2b0ab7b1ca4d7455ddd03d9d1ba07c8d2b77
Deborah Olusanya <deboraholusanya008@gmail.com>
2025-05-27 11:59:54 +0100

    Updated  2-arguments.js

---
+++ 2-arguements.js
@@ -0,0 +1,1 @@
+const args = process.argv.slice(2); if (args.length === 0) { console.log('No argument'); } else if (args.length === 1) { console.log('Arguements found'); } else { console.log('Arguements found'); }
