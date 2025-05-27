3b6c102e42fd7ab57577b4305bf06e8d61e6facc
Deborah Olusanya <deboraholusanya008@gmail.com>
2025-05-27 12:08:53 +0100

    Deleted  2-arguments.js

--- 2-arguements.js
+++
@@ -1,1 +0,0 @@
-const args = process.argv.slice(2); if (args.length === 0) { console.log('No argument'); } else if (args.length === 1) { console.log('Arguements found'); } else { console.log('Arguements found'); }
